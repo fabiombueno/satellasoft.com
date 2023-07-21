7fe4f4da061623ae4fd0886f4f80729ad06f5391
fabio.bueno <fabio.bueno@covre.com.br>
2023-07-21 15:14:07 -0300

    feito

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
 <?php
 echo "hello world";
 
-class Veiculo {
-public function initBuscarVeiculos()
-    {
-
-        $this->setConnection();
-        $this->setArray($data);
-        echo "hello world";
-        return $this->buscarVeiculos();
-
-    }
-    }
-
-    echo buscarVeiculos();
 ?>
