ea4c0039c9e97d9b07163a3d43417589ac7ebe9b
fabio.bueno <fabiomoreira.bueno@outlook.com>
2023-08-03 15:18:32 -0300

    Teste 03/08

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
+<!DOCTYPE html> <html lang="pt-br"> <head> <meta charset="UTF-8"> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<meta http-equiv="X-UA-Compatible" content="ie=edge"> 
+<title>Título da Página</title> </head>
+
+<body>
+
+</body> 
+
+
+
+
+</html>
+
+
+
 <?php
-echo "hello world";
+echo "hello worldt";
 
 ?>
