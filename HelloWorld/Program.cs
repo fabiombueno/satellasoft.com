3fe60c76e9485cd4d1acc923fac4abeb4dd228bf
fabio.bueno <fabio.bueno@covre.com.br>
2023-10-10 14:18:51 -0300

    C#

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //testee
+
+            Console.Write(5/10);
+        }
+    }
+}
